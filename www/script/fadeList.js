1bf63f43be3d86a772bcd25c35c09a95c590c2b3
Thibault <thibault@kapupa.fr>
2016-08-17 18:34:19 +0200

    [JS] add fadeIn/Out script

---
+++ www/script/fadeList.js
@@ -0,0 +1,28 @@
+<script type="text/javascript" language="JavaScript">
+
+
+function fadeIn(el){
+  el.className = "fade-In";
+  window.setTimeout(fadeOut,2000,el);
+}
+
+function fadeOut(el){
+  el.className = "fade-Out";
+  window.setTimeout(fadeNon,1000,el);
+}
+
+function fadeNon(el){
+  el.className  = "fade-el";
+  fadeTab(el);
+}
+
+function fadeTab(){
+  i = (i+1)%(fadelist.length);
+  fadeIn(fadelist[i]);
+}
+
+var fadelist = document.getElementsByClassName("fade-el");
+var i = 0;
+//Calls the init function on pageload.
+onload=fadeTab;
+</script>
