13b2004957400f9ae9fba2d28f1ab2828ddf85ee
Thibault <admin@kapupa.fr>
2016-09-08 16:04:54 +0200

    [Css][JS] add next adn previous button on diaporama

--- www/script/slide.js
+++ www/script/slide.js
@@ -1,38 +1,46 @@
 <script type="text/javascript" language="JavaScript">
 
-  function SlideIn(el){
-    el.classList.add("in");
-    window.setTimeout(SlideOut,2000,el);
-  }
-
-  function SlideOut(el){
-    if(el.classList.contains("right")) el.className = "diapo left";
-    else if(el.classList.contains("left")) el.className = "diapo right";
-    SlideTab();
-    //window.setTimeout(SlideT,1000,el);
-  }
-
   function GoToSlide(num){
-
+    el1 = Sladelist[i],
+    el2 = Sladelist[num];
+    if(el1.classList.contains("right")) el1.className = "diapo left";
+    else if(el1.classList.contains("left")) el1.className = "diapo right";
+    el2.classList.add("in");
+    i=num;
   }
 
-  function PreviousSlide(dir){
-    j=(i-1)%Sladelist.length;
+  function PreviousSlide(){
+    j=Math.abs(i-1)%Sladelist.length;
     GoToSlide(j);
   }
 
-  function NextSlide(dir){
+  function NextSlide(){
     j=(i+1)%Sladelist.length;
     GoToSlide(j);
   }
 
-  function SlideTab(){
-    if(i<Sladelist.length)  SlideIn(Sladelist[i++]);
+  function SlidePlay(){
+    if(i<Sladelist.length){
+      NextSlide();
+    }
+  }
+
+  function SlideStop(){
+    window.clearInterval(play);
+  }
+
+  function SlideStart(){
+    Sladelist[0].classList.add("in");
+    window.setTimeout(function(){
+      play = window.setInterval(SlidePlay,tempo);
+    },2*tempo);
   }
 
   var Sladelist = document.getElementsByClassName("diapo");
   var i = 0;
+  var tempo = 2000;
+  var play;
   //Calls the init function on pageload.
-  onload=SlideTab;
+  onload=SlideStart;
 
 </script>
