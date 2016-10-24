aa5abc6e81d0f6299d9cdecc0ab1ef0011d6b10a
Thibault <admin@kapupa.fr>
2016-10-25 01:14:19 +0200

    [FullStack] minor refactorig and fix

--- server.js
+++ server.js
@@ -11,22 +11,20 @@ var bunyan = require('bunyan'),
 					res: bunyan.stdSerializers.res
 				}
 		});
-//var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var colors = require('colors');
 
 
-//pages integration
-exp.use(express.static(conf.wwwFolder));
-//css integration
-exp.use(express.static(conf.cssFolder));
-//image integration
-exp.use(express.static(conf.picsFolder));
-//js  integration
-exp.use(express.static(conf.scriptFolder));
+//Static Folder
+//------------------------------------------------------------------------------
+exp.use(express.static(conf.wwwFolder));	 //pages integration
+exp.use(express.static(conf.cssFolder));   //css integration
+exp.use(express.static(conf.picsFolder));  //image integration
+exp.use(express.static(conf.scriptFolder));//js  integration
 
 
 //usefull multipurpose function
+//------------------------------------------------------------------------------
 function fileListToEjs(dir, callback){
 	fs.readdir(dir, function(err, files) {
 		callback(files);
@@ -43,29 +41,25 @@ function shuffle(array, callback) {
   callback(array);
 }
 
-//Horizontal navBar button
+//Horizontal navBar global var
+//------------------------------------------------------------------------------
 global.TitleNavBar = ['Home' ,'Team'	  ,'Contact' ];
 global.RefNavBar 	 = ['/'    ,'/people' ,'/contact'];
 global.TextNavBar  = ['	  ' ,'L\'équipe','Contact' ];
 global.ColorNavBar = ['black','blue'    ,'red'     ];
 
-//redirect ProTo to wwwFolder/home.html
-exp.get('/', function (req, res){
-res.render('../www/home.ejs',
- 					{ListDiapo : fs.readdirSync("www/diaporama"),
-					 ListSlide : fs.readdirSync("www/slides"),
-				 	 Title : TitleNavBar[0]
-				 	});
+//------------------------------------------------------------------------------
+//													Rooter Part
+//------------------------------------------------------------------------------
 
-/* FIXME : can't pass slides arguments
-		var slides = fs.readdirSync("www/slides");
-		fileListToEjs("www/diaporama", function(files,slides){
-				res.render('../www/home.ejs', {ListDiapo : files, ListSlide : slides});
-		});
-*/
+exp.get('/', function (req, res){
+	res.render('../www/home.ejs',
+	 					{ListDiapo : fs.readdirSync("www/diaporama"),
+						 ListSlide : fs.readdirSync("www/slides"),
+					 	 Title : TitleNavBar[0]
+					 	});
 });
 
-//redirect ProTo/member to wwwFolder/people.html
 exp.get('/people', function (req, res){
 	fileListToEjs("www/people",function(files){
 		shuffle(files,function(array){
@@ -77,19 +71,19 @@ exp.get('/people', function (req, res){
 	})
 });
 
-//redirect to contact form
 exp.get('/contact', function (req, res){
 	res.render('../www/contact.ejs',{Title : TitleNavBar[2]});
 });
 
-//redirect erroneous pages to 404
 exp.get('*', function (req, res){
-	res.render('../www/404.ejs');
+	res.render('../www/404.ejs', {Title : '404'});
 	log.warn('Demande d\'accès à page inéxistante (' + req.get('host') + req.originalUrl +
 						') par : ' + req.ip);
 });
 
-//Fonction d'écoute sur conf.listenInterface en conf.listenPort
+//------------------------------------------------------------------------------
+//										     Server Management Part
+//------------------------------------------------------------------------------
 exp.listen(conf.listenPort, conf.listenInterface,function(){
 	conf.info(function(fileconfig){log.debug(fileconfig)});
 	log.info( ': server running using port : '
